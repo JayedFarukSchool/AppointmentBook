ad13c5c29735b54a01978cf6676d4ae53aa6571b
JayedFaruk <jayedf@nycstudents.net>
2025-09-26 15:32:58 -0400

    page 4/7

---
+++ src/AppointmentBook.java
@@ -0,0 +1,32 @@
+public class AppointmentBook {
+    private boolean [][] schedule;
+
+    private boolean isMinuteFree(int period, int minute){
+        return schedule[period-1][minute];
+    }
+
+    public int findFreeBlock(int period, int duration) {
+        int block = 0;
+        for (int i = 0; i < 50; i++)
+            if (isMinuteFree(period, i))
+                block++;
+        if (block == duration)
+            return i - duration + 1;
+        else block = 0;
+        return -1;
+    }
+
+    public boolean makeAppointment(int startPeriod, int endPeriod, int duration){
+        return false;
+    }
+
+    public AppointmentBook(boolean[][] schedule){
+        this.schedule = schedule;
+    }
+
+
+    public void printPeriod(int period){
+        for (int i =0; i < schedule[period-1].length; i++)
+        System.out.println(i + " " + schedule[period-1][i]);
+
+    }
