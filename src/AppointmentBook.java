9a24b0a1217126944d1ac93af529b7f83ba45d26
JayedFaruk <jayedf@nycstudents.net>
2025-10-24 15:01:32 -0400

    first part of using text files

--- src/AppointmentBook.java
+++ src/AppointmentBook.java
@@ -1,50 +1,45 @@
 public class AppointmentBook {
-    private boolean [][] schedule;
+    private boolean[][] schedule;
 
-    private boolean isMinuteFree(int period, int minute){
-        return schedule[period-1][minute];
+    private boolean isMinuteFree(int period, int minute) {
+        return schedule[period - 1][minute];
     }
 
     public int findFreeBlock(int period, int duration) {
         int block = 0;
         for (int i = 0; i < 50; i++)
-            if (isMinuteFree(period, i))
-            {
+            if (isMinuteFree(period, i)) {
                 block++;
-            if (block == duration)
-                return i - duration + 1;
-            else block = 0;
+                if (block == duration)
+                    return i - duration + 1;
+                else block = 0;
             }
         return -1;
     }
 
-    public boolean reserveBlock(int period, int startMinute, int duration)
-    {
-        for(int i=startMinute; i<startMinute + duration; i++)
-        {
-            schedule[period-1][i] = false;
+    public boolean reserveBlock(int period, int startMinute, int duration) {
+        for (int i = startMinute; i < startMinute + duration; i++) {
+            schedule[period - 1][i] = false;
         }
         return false;
     }
 
-    public boolean makeAppointment(int startPeriod, int endPeriod, int duration){
-        for(int i = startPeriod; i<= endPeriod; i++) {
+    public boolean makeAppointment(int startPeriod, int endPeriod, int duration) {
+        for (int i = startPeriod; i <= endPeriod; i++) {
             int freeBlock = findFreeBlock(i, duration);
-            if (freeBlock > -1)
-            {
+            if (freeBlock > -1) {
                 reserveBlock(i, freeBlock, duration);
             }
         }
         return false;
     }
 
-    public AppointmentBook(boolean[][] schedule){
+    public AppointmentBook(boolean[][] schedule) {
         this.schedule = schedule;
     }
 
 
-    public void printPeriod(int period)
-    {
+    public void printPeriod(int period) {
         for (int i = 0; i < schedule[period - 1].length; i++)
             System.out.println(i + " " + schedule[period - 1][i]);
     }
